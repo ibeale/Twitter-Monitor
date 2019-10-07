8eb52575d178a7f088edc7b44658193bddab218d
Isaac Beale <ibeale1@gmail.com>
2019-10-07 16:38:54 -0700

    asynchronously make web requests

--- scraper.py
+++ scraper.py
@@ -1,26 +1,39 @@
 import requests
 import re
+import time
+import aiohttp
+import asyncio
 from bs4 import BeautifulSoup as bs
 
+# takes a url, and takes number of tweets to output
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
 
-def get_timeline(url):
-    # link to profile to monitor
-    data = requests.get(url)
-    all_tweets = []
-    html = bs(data.text, 'html.parser')
+async def get_recent(username, n):
+    base_link = 'https://twitter.com'
+    url = base_link + '/' + username
+    async with aiohttp.ClientSession() as session:
+        data_text = await fetch(session, url)
+    # data = requests.get(url)
+    recent_tweets = []
+    html = bs(data_text, 'html.parser')
     # get timeline
     timeline = html.select('#timeline li.stream-item')
 
-    # makes a file to see the exact html we're working with, but formatted
-    # nicely. Uncomment the next two lines to do so.
+    # DEBUG makes a file to see the exact html we're working with, but
+    # formatted nicely. Uncomment the next two lines to do so.
 
     # with open('html.html', 'w', encoding='utf-8') as f_out:
         # f_out.write(html.prettify())
 
-    for tweet in timeline:
+    for tweet in timeline[:n]:
         tweet_id = tweet['data-item-id']
         tweet_link = '{}'.format(url) + '/status/' + tweet_id
         tweet_text = tweet.select('p.tweet-text')[0].get_text()
+        tweet_text = tweet_text.replace(u'\xa0', u' ')
+        tweet_text = tweet_text.replace('https://', ' https://')
+        tweet_text = tweet_text.replace('\n', ' ')
 
         # selects all <a> tags with class='twitter-timeline-link'
         redir_html = tweet.select('p.tweet-text a.twitter-timeline-link')
@@ -39,16 +52,26 @@ def get_timeline(url):
         # output to a list of dictionaries
         if(found):
             pic_link = 'pic.twitter.com/{}'.format(found[0])
-            all_tweets.append({"id": tweet_id, "text": tweet_text, "link_to_tweet": tweet_link, "links": in_tweet_links, "link_to_pic": pic_link})
+            recent_tweets.append({"id": tweet_id, "text": tweet_text, "link_to_tweet": tweet_link, "links": in_tweet_links, "link_to_pic": pic_link})
         else:
-            all_tweets.append({"id": tweet_id, "text": tweet_text, "link_to_tweet": tweet_link, "links": in_tweet_links})
+            recent_tweets.append({"id": tweet_id, "text": tweet_text, "link_to_tweet": tweet_link, "links": in_tweet_links})
 
-    print(all_tweets)
+    print(recent_tweets)
+
+def main():
+    username = 'IsaacBeale2'
+    start_time = time.time()
+    loop = asyncio.get_event_loop()
+    all_groups = asyncio.gather(get_recent(username, 1), get_recent(username, 1), get_recent(username, 1), get_recent(username, 1), get_recent(username, 1))
+    results = loop.run_until_complete(all_groups)
+    # get_recent(username, 1)
+    # get_recent(username, 1)
+    # get_recent(username, 1)
+    print("Took %s seconds to execute" % (time.time() - start_time))
 
 
 if __name__ == '__main__':
-    url = 'https://twitter.com/IsaacBeale2'
-    get_timeline(url)
+    main()
 
 
 # TO DO:
